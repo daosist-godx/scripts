32ef23754e030d772005f1fb9766e0194cc60926
ivan <kasablanka1235@gmail.com>
2026-04-03 23:38:34 +0500

    Remove mistaken upload

--- EVIL-RDS shadow.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set /P rcomp="Enter name or IP of a Remote PC: "
-query session /server:%rcomp%
-set /P rid="Enter RDP user ID: "
-start mstsc /shadow:%rid% /v:%rcomp% /control /noConsentPrompt
